c892bef55e618f39f508bbafd62de8262596627b
Azra Shafi Ullah <164544626+AzraShafiullah1@users.noreply.github.com>
2025-02-17 20:34:02 +0000

    implemented a few Scientific functions

--- src/script.js
+++ src/script.js
@@ -10,35 +10,50 @@ document.addEventListener("DOMContentLoaded", function () {
 
         const convertedValue = currentValue
             .replace("x", "*")
-        
             .replace('%', '*0.01')
             .replace('÷', "/")
-      
-         
+            .replace('sin', 'Math.sin')
+            .replace('cos', 'Math.cos')
+            .replace('ln', 'Math.log')
+            .replace('π', 'Math.PI')
+            .replace('log', 'Math.log10')
+            .replace('e', 'Math.E')
+            .replace('tan', 'Math.tan')
+            .replace('√', 'Math.sqrt');
 
-       console.log('convertedValue', convertedValue)
-    
+
+        console.log('convertedValue', convertedValue)
+
         const result = eval(convertedValue);
         currentValue = result.toString();
         display.value = currentValue;
 
-       }
+    }
 
     for (let i = 0; i < buttons.length; i++) {
         const button = buttons[i];
         button.addEventListener('click', function () {
             const value = button.innerText;
 
-            if (value == "CE") {
-                currentValue = "";
+            try {
+                if (value == "AC") {
+                    currentValue = "";
+                    display.value = currentValue
+                } else if (value == "=") {
+                    evaluateResult();
+                } else {
+                    currentValue += value;
+                    display.value = currentValue;
+                }
+
+            } catch (error) {
+                console.error(error);
+                currentValue = "ERROR"
                 display.value = currentValue
-            } else if (value == "=") {
-                evaluateResult();
-            } else {
-              currentValue += value;
-              console.log('currentValue:', currentValue);
-              display.value = currentValue;
+
+
             }
+
 
         })
     }
